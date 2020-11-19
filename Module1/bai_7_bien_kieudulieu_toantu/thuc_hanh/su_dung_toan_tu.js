272396c7e4064bf6e8e412d05595879d46b75be2
MAIDOAN2302 <doanthimai23021992@gmail.com>
2020-11-19 21:22:42 +0700

    up bài 7

---
+++ Module1/bai_7_bien_kieudulieu_toantu/thuc_hanh/su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt(" enter the width");
+inputheight = prompt(" enter the height");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height ;
+document.write(" the area is " + area);
