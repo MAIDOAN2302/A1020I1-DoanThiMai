3584815cda9462f5b359b9e5cee77e1c680dc160
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-01-17 15:56:30 +0700

    array1

---
+++ Module2/src/_03_Array_in_Java/bai_tap/SumValueOfOneColumn.java
@@ -0,0 +1,42 @@
+package _03_Array_in_Java.bai_tap;
+
+import java.util.Scanner;
+
+public class SumValueOfOneColumn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input size of array1: ");
+        int size1 = sc.nextInt();
+
+        System.out.println("Input size of array2: ");
+        int size2 = sc.nextInt();
+
+        int[][] arr = new int[size1][size2];
+        System.out.println("Input element of arr: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matris From : ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(" " + arr[i][j]);
+            }
+            System.out.println(" ");
+        }
+        int sum = 0;
+        int location;
+        System.out.println("Input location of column you want calculator sum: ");
+        location = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (j == location) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.println("Sum value of column is: " + sum);
+    }
+}
