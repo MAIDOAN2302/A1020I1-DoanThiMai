42b59ccb6ba8e35d434f33c3d79833c229203dd0
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-01-20 20:42:16 +0700

    Access modifier

---
+++ Module2/src/_05_Modifier/bai_tap/Circle.java
@@ -0,0 +1,50 @@
+package _05_Modifier.bai_tap;
+
+public class Circle {
+    private final double radius = 1.0;
+    private final String color = "red";
+
+    public Circle() {
+    }
+
+    public double HinhTron(double radius) {
+        return this.radius;
+    }
+
+        public double getRadius() {
+
+        return this.radius;
+    }
+
+    public double getArea() {
+
+        return this.radius * this.radius * Math.PI;
+    }
+//    protected double getRadius() {
+//
+//        return this.radius;
+//    }
+//
+//    protected double getArea() {
+//
+//        return this.radius * this.radius * Math.PI;
+//    }
+//     double getRadius() {
+//
+//        return this.radius;
+//    }
+//
+//     double getArea() {
+//
+//        return this.radius * this.radius * Math.PI;
+//    }
+//    private double getRadius() {
+//
+//        return this.radius;
+//    }
+//
+//    private double getArea() {
+//
+//        return this.radius * this.radius * Math.PI;
+//    }
+}
---
+++ Module2/src/_05_Modifier/thuc_hanh/static_methods/TestStaticProperty.java
@@ -0,0 +1,10 @@
+package _05_Modifier.thuc_hanh.static_methods;
+
+public class TestStaticProperty {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+        System.out.println(car1.numberOfCars);
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+        System.out.println(car2.numberOfCars);
+    }
+}
