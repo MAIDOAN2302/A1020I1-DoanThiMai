55567ff6140fee8f036a3036f2b1737cc74664f6
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-04-01 10:35:57 +0700

    Update

--- Module3/bai1_database/bai_tap/bai_tap1.sql
+++ Module3/bai1_database/bai_tap/bai_tap1.sql
@@ -2,10 +2,36 @@ create database student_management;
 use student_management;
 
 create table student(
-id int primary key,
+id int primary key auto_increment,
  `name`varchar(50),
  date_of_birth date
  );
- insert into student
+ insert into student(`name`, date_of_birth)
 
-values (1, 'Tiến', '2021-03-31');
+values 
+('Tiến', '2021-03-31'),
+('Mân', '1998-03-31'),
+('Trường', '2000-03-31'),
+('Thanh', '1991-03-31');
+
+select `name`
+from student
+where `name` = 'Thanh';
+
+delete from student
+where id = 2;
+
+update student
+set `name` = 'Mai'
+where `name` = 'Thanh';
+
+alter table student
+add column email varchar(30);
+
+ 
+insert into student (email)
+value ('abc@abc')
+
+
+
+drop table student;
