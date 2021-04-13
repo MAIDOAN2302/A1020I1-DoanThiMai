286504527fdf47e325c2daee4a42784a46718155
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-04-13 22:32:50 +0700

    update lesson 6 : CSS

--- Module3/casestudy/database/yeu_cau_16.sql
+++ Module3/casestudy/database/yeu_cau_16.sql
@@ -1,8 +1,9 @@
 -- Yêu cầu 16:  Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2017 đến năm 2019.
-
+SET SQL_SAFE_UPDATES = 0;
 delete from nhanvien NV
 where NV.idNhanVien not in (
 select hopdong.idNhanVien
 from hopdong
 where year(ngayLamHopDong) between 2017 and 2019
 );
+SET SQL_SAFE_UPDATES = 1;
