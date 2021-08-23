74c297921c766909129dbc8280b2398a3f7c418b
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-08-23 20:56:09 +0700

    font-size-editor

---
+++ Module5/03_Angular_Overview/thuc_hanh/font-size-chane/src/app/font-size-editor/font-size-editor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FontSizeEditorComponent } from './font-size-editor.component';
+
+describe('FontSizeEditorComponent', () => {
+  let component: FontSizeEditorComponent;
+  let fixture: ComponentFixture<FontSizeEditorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FontSizeEditorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FontSizeEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Module5/03_Angular_Overview/thuc_hanh/font-size-chane/src/app/font-size-editor/font-size-editor.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-font-size-editor',
+  templateUrl: './font-size-editor.component.html',
+  styleUrls: ['./font-size-editor.component.css']
+})
+export class FontSizeEditorComponent implements OnInit {
+  fontSize = 10;
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+  onChange(value: any) {
+    this.fontSize = value;
+  }
+}
