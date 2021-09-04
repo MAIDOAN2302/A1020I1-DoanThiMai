21882b84c8752a7b82d8035a024d45ec6350cbdf
MAIDOAN2302 <doanthimai23021992@gmail.com>
2021-09-04 17:32:43 +0700

    module-DI

---
+++ Module5/08_Modules_Dependency_Injection/thuc_hanh/modules-DI/src/app/content-projection/cp-card/cp-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CpCardComponent } from './cp-card.component';
+
+describe('CpCardComponent', () => {
+  let component: CpCardComponent;
+  let fixture: ComponentFixture<CpCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CpCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CpCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Module5/08_Modules_Dependency_Injection/thuc_hanh/modules-DI/src/app/content-projection/cp-card/cp-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cp-card',
+  templateUrl: './cp-card.component.html',
+  styleUrls: ['./cp-card.component.css']
+})
+export class CpCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
